ad6e30c30911aa525b3185a8d5b59ebca7570348
kooroshg1 <kooroshg1@gmail.com>
2015-10-18 15:46:05 -0400

    Upload the base version

---
+++ working_version/generateCircle.m
@@ -0,0 +1,23 @@
+clc;
+% clear all;
+close all;
+format short g;
+% ----------------------------------------------------------------------- %
+xc = 0.5;
+yc = 0.5;
+dR = 0.000000;
+R = 0.10 - dR;
+N = 100;
+
+x = xc + R * cos(linspace(0,2*pi,N)'); %x(end) = [];
+y = yc + R * sin(linspace(0,2*pi,N)'); %y(end) = [];
+
+pointCloud = [x y];
+
+dxdR = cos(linspace(0,2*pi,N)'); %dxdR(end) = [];
+dydR = sin(linspace(0,2*pi,N)'); %dydR(end) = [];
+
+pointCloudSensitivity = [dxdR dydR];
+
+dlmwrite('point_cloud.txt',pointCloud);
+dlmwrite('point_cloud_sensitivity.txt',pointCloudSensitivity);
